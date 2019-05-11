6ad9a4156f789f375f6e740724657d9bbfbec23f
涛 孙 <BestSunTao@outlook.com>
2019-05-11 20:47:08 +0800

    第三次推送

---
+++ mfc3/resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 mfc3.rc 使用
+//
+#define IDD_MFC3_DIALOG                 102
+#define IDR_MAINFRAME                   128
+#define IDC_TREE1                       1000
+#define IDC_LISTBOX                     1002
+#define BTN_CLEAR                       1003
+#define IDC_LISTCTRL                    1004
+#define IDC_NAME                        1006
+#define IDC_SUBJECT1                    1007
+#define IDC_ID                          1008
+#define IDC_SUBJECT2                    1009
+#define IDC_SUBJECT3                    1010
+#define BTN_ADD                         1011
+#define BTN_RESET                       1012
+#define IDC_COMBO_GRADE                 1013
+#define IDC_EDIT1                       1014
+#define IDC_AVERAGE                     1014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
